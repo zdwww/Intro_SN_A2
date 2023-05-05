c4b54f5bcbe655326f416ec09e4ec0d002144b4a
zdwww <54753956+zdwww@users.noreply.github.com>
2023-05-05 18:37:05 +0200

    update R

--- a2.R
+++ a2.R
@@ -9,6 +9,7 @@ getwd()
 # data(package = "networkdata")
 covert_28 # London Gang N54E315
 vertex_attr(covert_28)
+table(E(covert_28)$weight)
 
 covert_24 # Italian Gang N67E114
 vertex_attr(covert_24)
@@ -16,12 +17,16 @@ vertex_attr(covert_24)
 
 
 #### Task 1.(a) Describe and visualize the dataset
-
-ggraph(covert_24, layout = 'fr') + 
-  geom_node_point() + 
+vertex_attr(covert_24, "country") <- as.character(vertex_attr(covert_24, "country"))
+set.seed(10)
+layout.graph <- create_layout(covert_24, 
+                              layout = 'fr')
+ggraph(layout.graph) + 
   geom_edge_link() + 
+  geom_node_point(aes(colour = country), size=3) + 
   theme_graph() 
 
 #### Task 2.(a) Community detection
+
 
 #### Task 2.(b) Describe and visualize the dataset
